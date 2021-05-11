6653924637ece3ad375fc72c84374e1d586c0c38
Adroaldo <hygino@msn.com>
2021-05-11 15:37:34 -0300

    segundo teste

--- src/main/java/com/anotacoes/LombokExemploApplication.java
+++ src/main/java/com/anotacoes/LombokExemploApplication.java
@@ -1,13 +1,24 @@
 package com.anotacoes;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import entities.Product;
 
 @SpringBootApplication
 public class LombokExemploApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LombokExemploApplication.class, args);
-	}
+		// SpringApplication.run(LombokExemploApplication.class, args);
+		Product p1 = new Product(1L, "TV", 900.45);
+		p1.setName("Television");
+		// Product p2 = new Product(2L, "Notebook", 2367.16);
+		System.out.println(p1);
+		System.out.println(p1.getName());
+		System.out.println(p1.hashCode());
+		System.out.println(p1.getClass());
+		System.out.println(p1.getId());
 
+		Product p2 = Product.builder().name("Tablet").price(45.99).id(3L).build();
+		System.out.println(p2);
+	}
 }
--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class Product {
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*@Getter//gerra dos getters
+@Setter//gera os setter
+@EqualsAndHashCode//gera hashcode and equals
+
+@ToString//gera toString*/
+@Data
+/*All together now: A shortcut for @ToString, @EqualsAndHashCode,
+ *  @Getter on all fields, and @Setter on all non-final fields, and @RequiredArgsConstructor!
+ */
+@NoArgsConstructor //construtor sem argumentos
+@AllArgsConstructor//construtor com dodos os argumentos
+@Builder//outra maneira de usar construtor
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
 
 }
