72c091b4b2acd2f8e13f2413a5e4cbfc80609278
Adroaldo <hygino@msn.com>
2021-05-11 14:57:31 -0300

    Criando projeto

---
+++ src/main/java/com/anotacoes/LombokExemploApplication.java
@@ -0,0 +1,13 @@
+package com.anotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anotacoes/LombokExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
